f641b46bc144d4824061288169711a2f37aaddb2
Avisa Poshtkouhi <avisa@omniscent.com>
2025-02-18 20:03:17 -0800

    feat: react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 
-
+import { Route, Routes } from "react-router"
 import './App.css'
 import Footer from './components/Footer/Footer'
 import Fruits from './components/Fruits/Fruits'
 import Navbar from './components/NavBar/Navbar'
+import About from "./components/About/About"
+import Contact from "./components/Contact/Contact"
 
 function App() {
 
@@ -11,7 +13,11 @@ function App() {
   return (
     <>
       <Navbar />
-      <Fruits />
+      <Routes>
+        <Route path="/" element={<Fruits />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
       <Footer />
     </>
   )
--- src/components/NavBar/Navbar.jsx
+++ src/components/NavBar/Navbar.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import "./NavBar.css"
 function Navbar() {
     return (
-        <div className="navbar-logo">Fruitopia 🍓🍉🍒🍎🍑</div>
+        <div className="navbar">
+            <div className="navbar-logo">Fruitopia 🍓🍉🍒🍎🍑</div>
+            <div className="navbar-menu">
+                <NavLink to="/">Home</NavLink>
+                <NavLink to="/about">About</NavLink>
+                <NavLink to="/contact">Contact</NavLink>
+            </div>
+        </div>
     )
 }
 
